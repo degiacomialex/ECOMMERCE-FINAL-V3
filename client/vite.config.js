be8c9952ddd6db58b321f0153710c332776df3d6
AlexanderDeGiacomi <degiacomialexander@gmail.com>
2025-11-07 00:40:39 -0300

    fix: Build final con ruta base para GitHub Pages

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  // 💥 FIX CRÍTICO: Se añade la propiedad 'base' con el nombre del repositorio 💥
+  base: '/ECOMMERCE-FINAL-V3/', 
 })
